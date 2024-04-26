ab810d520456aa367982c675feaae0621d774a87
yadhavan <93464775+user-name-yad@users.noreply.github.com>
2024-04-26 12:03:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,11 @@ app.use(cors(
 ));
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.get('/', (req, res) => {
